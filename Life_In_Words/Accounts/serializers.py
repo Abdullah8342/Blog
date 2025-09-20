732fe512f90247414a5136b1a66c952871fcfee0
Abdulah <117518706+Abdullah8342@users.noreply.github.com>
2025-09-20 16:12:17 +0500

    Delete Life_In_Words directory

--- Life_In_Words/Accounts/serializers.py
+++
@@ -1,7 +0,0 @@
-from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
-
-class UserCreateSerializer(BaseUserCreateSerializer):
-    '''USER CREATE SERIALIZERS'''
-    class Meta(BaseUserCreateSerializer.Meta):
-        '''CUSTOM SERAILIZERS'''
-        fields = ['id','username','first_name','last_name','password','email']
--- Life_In_Words/Life_Stories/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LifeStoriesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Life_Stories'
